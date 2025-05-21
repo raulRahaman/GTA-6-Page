4a48b33cacb752898ac7fd80ec4ba3d26dfb909b
Raul <raulrahaman2024@gmail.com>
2025-05-21 20:53:59 +0100

    Further improvements to UI as well as giving a msg to the button

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
+const overlay = document.querySelector('.overlay');
+const msg = document.querySelector('.preorder-msg')
+const preorderBtn = document.querySelector('.btn');
+const closeMsg = document.querySelector('.close-msg');
+
+preorderBtn.addEventListener('click', function(e){
+  e.preventDefault();
+  msg.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+})
+
+closeMsg.addEventListener('click', function(){
+  msg.classList.add('hidden');
+  overlay.classList.add('hidden');
+})
+
+
+
+
+
 document.querySelector(".nav__links").addEventListener("click", function (el) {
   el.preventDefault();
 
   if (el.target.classList.contains("nav__link")) {
     el.target.style.backgroundColor = "var(--color-fifth)";
-    console.log(el.target);
+    console.log(el.target.closest('.slogan'));
   }
 });
